2f9465b44ee5c2674fdcf440a606d85957daabd2
shamskhodary <shams.khodary@gmail.com>
2022-09-21 22:14:57 +0300

    created header jsx and css relates #4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,44 @@
 import { Component } from "react";
 import "../css/Header.css"
+import { getAllPokemon } from "../utils";
 
 export default class Header extends Component {
+  state = {
+    data: null,
+    searchPokemon: "",
+  }
+
+  componentDidMount() {
+    getAllPokemon().then((res) => {
+      this.setState(({ data: res }))
+    })
+  }
+
+  handleSearch = (e) => {
+    this.setState({ searchPokemon: e.target.value.toLowerCase() });
+  }
   render() {
-    return <div>Header</div>
+    const { data } = this.state;
+
+    if (!data) return <div>Loading...</div>
+    console.log(this.state.searchPokemon);
+    return (
+      <header>
+        <div>
+          <img src="../../logo.png" alt="" />
+        </div>
+        <input type="search" placeholder="Search Pokemon..." onChange={this.handleSearch} />
+        {/* {data.filter((e) => this.state.searchPokemon === "" ? e ? e.name.toLowerCase().includes(this.state.searchPokemon) : console.log(e) : 'ttt')} */}
+        {/* {data.filter(poke => {
+          if (this.state.searchPokemon === "") {
+            return poke
+          } else if (poke.name.includes(this.state.searchPokemon)) {
+            console.log(poke);
+            return poke
+          }
+        })
+        } */}
+      </header>
+    )
   }
 }
