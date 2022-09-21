5f6897e6a997e0a967ddf46c0edb8bb19772650b
shamskhodary <shams.khodary@gmail.com>
2022-09-21 22:08:32 +0300

    create card section layout and fetched data from api relates #3

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,22 @@ import { Component } from "react";
 import '../css/Card.css'
 
 export default class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pokemon: props.pokemon,
+    }
+  }
   render() {
-    return <h1>Hello Batikha</h1>
+    const { pokemon } = this.state;
+    return (
+      <div className="cards" key={pokemon.id}>
+        <img src={pokemon.sprites.other.dream_world.front_default} alt="" />
+        <h1>{pokemon.name}</h1>
+        <span>Weight: {pokemon.weight}</span>
+        <br />
+        <span>Height: {pokemon.height}</span>
+      </div>
+    )
   }
 }
